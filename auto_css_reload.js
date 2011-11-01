35fec92a473ffe49f83c7d5a2412eb7118aa956d
Stephen Korecky <skorecky@gmail.com>
2011-11-01 18:06:44 -0500

    found a way to no longer have flickering on the screen caused by removing and adding the stylesheets

--- auto_css_reload.js
+++ auto_css_reload.js
@@ -1,20 +1,10 @@
 var reloadStyles = function(event){
   var styleSheets = document.querySelectorAll("link[rel='stylesheet']");
   for (var i = styleSheets.length - 1; i >= 0; i--){
-    // Remove stylesheets first
-    var style = styleSheets[i];
-    var head = document.querySelector("head");
-    head.removeChild(style);
-    
-    // Add them back in
-    var stylesheet = document.createElement('link');
+    var stylesheet = styleSheets[i];
     var queryString = '?reload=' + new Date().getTime();
-    newHref = style.attributes.href.value.replace(/\?.*|$/, queryString);
-    stylesheet.setAttribute('href', newHref);
-    stylesheet.setAttribute('rel', style.rel);
-    stylesheet.setAttribute('type', style.type);
-    stylesheet.setAttribute('media', style.media);
-    head.appendChild(stylesheet);
+    var newHref = stylesheet.attributes.href.value.replace(/\?.*|$/, queryString);
+    stylesheet.attributes.href.value = newHref;
   };
 };
 
